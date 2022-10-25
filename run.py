9c1c25ac757f05ed8f8a632f4c912b24bfa25478
kevinmcsherry <kevinmcsherry01@gmail.com>
2022-10-25 21:08:47 +0000

    Added function to calculate star sign based on date entered for DOB

--- run.py
+++ run.py
@@ -19,25 +19,54 @@ tprint("Welcome" + "  " + "to" + "  " + "your" + "  " + "Daily")
 tprint("HOROSCOPE" + "   " + "Page")
 
 name = input("Please Enter your Name:")
+year = int(input(" Please enter the year you were born: "))
+month = int(input(" Please enter the number of the month you were born. For example 3 = March: "))
+day = int(input(" Please enter the day you were born "))
+dob = datetime.datetime(year,month,day)
 
 
-def calc_age():
-    Year = int(input(" Please enter the year you were born "))
-    Month = int(input(" Please enter the number of the month you were born.  For example 3 = March "))
-    Day = int(input(" Please enter the day you were born "))
-    DOB = datetime.datetime(Year,Month,Day)
-    Age = (datetime.datetime.now() - DOB)
+def calc_age(dob):
+    Age = (datetime.datetime.now() - dob)
     convertdays = int(Age.days)
     ExactAgeYears = convertdays/365
     AgeYears = int(ExactAgeYears)
-    print("You are" + str(AgeYears) + " years old")
+    print(str(name) + " You are " + str(AgeYears) + " years old")
+
+def calc_star_sign(day, month):
+    if month == 12:
+	    astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
+    elif month == 1:
+	    astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
+    elif month == 2:
+	    astro_sign = 'Aquarius' if (day < 19) else 'pisces'
+    elif month == 3:
+	    astro_sign = 'Pisces' if (day < 21) else 'aries'
+    elif month == 4:
+	    astro_sign = 'Aries' if (day < 20) else 'taurus'
+    elif month == 5:
+	    astro_sign = 'Taurus' if (day < 21) else 'gemini'
+    elif month == 6:
+	    astro_sign = 'Gemini' if (day < 21) else 'cancer'
+    elif month == 7:
+	    astro_sign = 'Cancer' if (day < 23) else 'leo'
+    elif month == 8:
+        astro_sign = 'Leo' if (day < 23) else 'virgo'
+    elif month == 9:
+	    astro_sign = 'Virgo' if (day < 23) else 'libra'
+    elif month == 10:
+	    astro_sign = 'Libra' if (day < 23) else 'scorpio'
+    elif month == 11:
+	    astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
+    print("Your Astrological sign is : " + str(astro_sign))
 
 
-calc_age()
+calc_age(dob)
+calc_star_sign(day, month)
 
 
-aries = SHEET.worksheet('Aries')
 
-data = aries.get_all_values()
+#aries = SHEET.worksheet('Aries')
 
-print(data)
+#data = aries.get_all_values()
+
+#print(data)
