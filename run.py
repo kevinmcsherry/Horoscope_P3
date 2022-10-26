5887109ec2cf5e0ecd33cc8c97133ae7be7820ee
kevinmcsherry <kevinmcsherry01@gmail.com>
2022-10-26 21:01:31 +0000

    added function to ask user if they want thier horoscope reading

--- run.py
+++ run.py
@@ -13,6 +13,19 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Horoscope_P3')
+aries = SHEET.worksheet('Aries')
+taurus = SHEET.worksheet('Taurus') 
+gemini = SHEET.worksheet('Gemini')
+cancer = SHEET.worksheet('Cancer')
+leo = SHEET.worksheet('Leo')
+virgo = SHEET.worksheet('Virgo')
+libra = SHEET.worksheet('Libra')
+scorpious = SHEET.worksheet('Scorpious')
+sagittarius = SHEET.worksheet('Sagittarius')
+capricorn = SHEET.worksheet('Capricorn')
+aquarius = SHEET.worksheet('Aquarius')
+pisces = SHEET.worksheet('Pisces')
+
 
 
 tprint("Welcome  to", font="small")
@@ -41,6 +54,7 @@ def calc_age(dob):
 
 
 def calc_star_sign(day, month):
+    global astro_sign
     if month == 12:
         astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
     elif month == 1:
@@ -69,21 +83,86 @@ def calc_star_sign(day, month):
     print(str(name) + ", you are " + str(AgeYears) + " years old \n"
          + "and " + "Your Astrological sign is : " + str(astro_sign))
 
-#def get_reading(astro_sign):
+
+def get_today_reading():
+    want_reading = str(input("Would you like your horoscope for today ? (Y/N): ")).lower().strip()
+    try:
+        if want_reading[0] == 'y':
+            return True
+            #add retrieve reading
+        elif want_reading[0] == 'n':
+            return False
+        else:
+            print('Invalid Input')
+            return get_today_reading()
+    except Exception as error:
+        print("Please enter valid inputs 'y' or 'n'")
+        print(error)
+        return get_today_reading()
 
 
-#def get_another_reading():
 
 
-#def get_new_reading():
+def aries_horoscope():
+    todays_scope = aries.acell('B2').value
+    print(todays_scope)
+
+
+def taurus_horoscope():
+    todays_scope = taurus.acell('B2').value
+    print(todays_scope)
+
+
+def gemini_horoscope():
+    todays_scope = gemini.acell('B2').value
+    print(todays_scope)
+
+
+def cancer_horoscope():
+    todays_scope = cancer.acell('B2').value
+    print(todays_scope)
+
+
+def leo_horoscope():
+    todays_scope = leo.acell('B2').value
+    print(todays_scope)
+
+
+def virgo_horoscope():
+    todays_scope = virgo.acell('B2').value
+    print(todays_scope)
+
+
+def libra_horoscope():
+    todays_scope = libra.acell('B2').value
+    print(todays_scope)
+
+
+def scorpious_horoscope():
+    todays_scope = scorpious.acell('B2').value
+    print(todays_scope)
+
+
+def sagittarius_horoscope():
+    todays_scope = sagittarius.acell('B2').value
+    print(todays_scope)
+
+
+def capricorn_horoscope():
+    todays_scope = capricorn.acell('B2').value
+    print(todays_scope)
+
+
+def aquarius_horoscope():
+    todays_scope = aquarius.acell('B2').value
+    print(todays_scope)
+
+
+def pisces_horoscope():
+    todays_scope = pisces.acell('B2').value
+    print(todays_scope)
+
 
 calc_age(dob)
 calc_star_sign(day, month)
-
-
-
-#aries = SHEET.worksheet('Aries')
-
-#data = aries.get_all_values()
-
-#print(data)
+get_today_reading()
