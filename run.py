a1af2e5e54122d0a27511cf99f0fe7f326e962a9
kevinmcsherry <kevinmcsherry01@gmail.com>
2022-10-26 14:13:47 +0000

    Changing font size and linage to fit better on deployed screen

--- run.py
+++ run.py
@@ -15,8 +15,11 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Horoscope_P3')
 
 
-print("Welcome" + " " + "to" + " " + "your" + " " + "Daily")
-print("Horoscope" + " " + "Page!\n")
+tprint("Welcome", font="small")
+tprint("to your", font="small")
+tprint("Daily", font="small")
+tprint("Horoscope", font="small")
+tprint("Page!\n", font="small")
 
 name = input("Please Enter your Name:")
 year = int(input(" Please enter the year you were born: "))
