c99b3d891c6ba6ed870c8a857f98b42fd70beb0d
kevinmcsherry <kevinmcsherry01@gmail.com>
2022-10-26 14:59:13 +0000

    added new line in message for age and star sign for better visibility

--- run.py
+++ run.py
@@ -42,32 +42,40 @@ def calc_age(dob):
 
 def calc_star_sign(day, month):
     if month == 12:
-	    astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
+        astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
     elif month == 1:
-	    astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
+        astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
     elif month == 2:
-	    astro_sign = 'Aquarius' if (day < 19) else 'pisces'
+        astro_sign = 'Aquarius' if (day < 19) else 'pisces'
     elif month == 3:
-	    astro_sign = 'Pisces' if (day < 21) else 'aries'
+        astro_sign = 'Pisces' if (day < 21) else 'aries'
     elif month == 4:
-	    astro_sign = 'Aries' if (day < 20) else 'taurus'
+        astro_sign = 'Aries' if (day < 20) else 'taurus'
     elif month == 5:
-	    astro_sign = 'Taurus' if (day < 21) else 'gemini'
+        astro_sign = 'Taurus' if (day < 21) else 'gemini'
     elif month == 6:
-	    astro_sign = 'Gemini' if (day < 21) else 'cancer'
+        astro_sign = 'Gemini' if (day < 21) else 'cancer'
     elif month == 7:
-	    astro_sign = 'Cancer' if (day < 23) else 'leo'
+        astro_sign = 'Cancer' if (day < 23) else 'leo'
     elif month == 8:
         astro_sign = 'Leo' if (day < 23) else 'virgo'
     elif month == 9:
-	    astro_sign = 'Virgo' if (day < 23) else 'libra'
+        astro_sign = 'Virgo' if (day < 23) else 'libra'
     elif month == 10:
-	    astro_sign = 'Libra' if (day < 23) else 'scorpio'
+        astro_sign = 'Libra' if (day < 23) else 'scorpio'
     elif month == 11:
-	    astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
+        astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
     print("")
-    print(str(name) + ", you are " + str(AgeYears) + " years old " + "and " + "Your Astrological sign is : " + str(astro_sign))
+    print(str(name) + ", you are " + str(AgeYears) + " years old \n"
+         + "and " + "Your Astrological sign is : " + str(astro_sign))
 
+#def get_reading(astro_sign):
+
+
+#def get_another_reading():
+
+
+#def get_new_reading():
 
 calc_age(dob)
 calc_star_sign(day, month)
