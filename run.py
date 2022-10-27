04b38d7e4ebd925f56b1f30e7be14a1f7a676e5b
kevinmcsherry <kevinmcsherry01@gmail.com>
2022-10-27 22:29:59 +0000

    Added new function to retreive correct data from spreadhseet based on star sign

--- run.py
+++ run.py
@@ -25,6 +25,8 @@ sagittarius = SHEET.worksheet('Sagittarius')
 capricorn = SHEET.worksheet('Capricorn')
 aquarius = SHEET.worksheet('Aquarius')
 pisces = SHEET.worksheet('Pisces')
+global astro_sign
+global want_reading
 
 tprint("Welcome  to", font="small")
 tprint("your  Daily", font="small")
@@ -47,19 +49,19 @@ def calc_age():
     Function to determine and calculate the exact age from
     the user based on date of birth entered
     """
-    Age = (datetime.datetime.now() - dob)
-    convertdays = int(Age.days)
-    ExactAgeYears = convertdays/365
-    global AgeYears
-    AgeYears = int(ExactAgeYears)
+    age = (datetime.datetime.now() - dob)
+    convertdays = int(age.days)
+    exact_age_years = convertdays/365
+    global age_years
+    age_years = int(exact_age_years)
 
 
-def calc_star_sign(day, month):
+def calc_star_sign():
     """
     Function to determine the star sign of the user based
     on the date of birth entered
     """
-    global astro_sign
+
     if month == 12:
         astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
     elif month == 1:
@@ -85,127 +87,77 @@ def calc_star_sign(day, month):
     elif month == 11:
         astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
     print("")
-    print(str(name) + ", you are " + str(AgeYears) + " years old \n"
+    print(str(name) + ", you are " + str(age_years) + " years old \n"
           + "and " + "Your Astrological sign is : " + str(astro_sign))
 
 
-def get_today_reading():
+def get_reading():
     """
     Function to ask the user if they would like
     thier horoscope for today
     """
+
     want_reading = str(input("Would you like your horoscope for today ? (Y/N): ")).lower().strip()
     try:
         if want_reading[0] == 'y':
-            return True
+            retrieve_today_reading()
         elif want_reading[0] == 'n':
-            return False
+            print("OK, GoodBye")
+            exit()
         else:
             print('Invalid Input')
-            return get_today_reading()
+            return get_reading()
     except Exception as error:
         print("Please enter valid inputs 'y' or 'n'")
         print(error)
-        return get_today_reading()
+        return get_reading()
 
 
-def aries_horoscope():
+def retrieve_today_reading():
     """
-    Function to determine and retrieve the cell value for Aries sign
-
+    Function to retrieve and display 
+    the correct horoscope reading
     """
-    todays_scope = aries.acell('B2').value
-    print(todays_scope)
-
-
-def taurus_horoscope():
-    """
-    Function to determine and retrieve the cell value for Taurus sign
-    """
-    todays_scope = taurus.acell('B2').value
-    print(todays_scope)
-
-
-def gemini_horoscope():
-    """
-    Function to determine and retrieve the cell value for Gemini sign
-    """
-    todays_scope = gemini.acell('B2').value
-    print(todays_scope)
-
-
-def cancer_horoscope():
-    """
-    Function to determine and retrieve the cell value for Cancer sign
-    """
-    todays_scope = cancer.acell('B2').value
-    print(todays_scope)
-
-
-def leo_horoscope():
-    """
-    Function to determine and retrieve the cell value for Leo sign
-    """
-    todays_scope = leo.acell('B2').value
-    print(todays_scope)
-
-
-def virgo_horoscope():
-    """
-    Function to determine and retrieve the cell value for Virgo sign
-    """
-    todays_scope = virgo.acell('B2').value
-    print(todays_scope)
-
-
-def libra_horoscope():
-    """
-    Function to determine and retrieve the cell value for Libra sign
-    """
-    todays_scope = libra.acell('B2').value
-    print(todays_scope)
-
-
-def scorpious_horoscope():
-    """
-    Function to determine and retrieve the cell value for Scorpious sign
-    """
-    todays_scope = scorpious.acell('B2').value
-    print(todays_scope)
-
-
-def sagittarius_horoscope():
-    """
-    Function to determine and retrieve the cell value for Sagittarius sign
-    """
-    todays_scope = sagittarius.acell('B2').value
-    print(todays_scope)
-
-
-def capricorn_horoscope():
-    """
-    Function to determine and retrieve the cell value for Capricorn sign
-    """
-    todays_scope = capricorn.acell('B2').value
-    print(todays_scope)
-
-
-def aquarius_horoscope():
-    """
-    Function to determine and retrieve the cell value for Aquarius sign
-    """
-    todays_scope = aquarius.acell('B2').value
-    print(todays_scope)
-
-
-def pisces_horoscope():
-    """
-    Function to determine and retrieve the cell value for Pisces sign
-    """
-    todays_scope = pisces.acell('B2').value
-    print(todays_scope)
+    astro_sign = 'aries'
+    if astro_sign == 'aries':
+        get_reading = aries.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'taurus':
+        get_reading = taurus.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'gemini':
+        get_reading = gemini.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'cancer':
+        get_reading = cancer.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'leo':
+        get_reading = leo.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'virgo':
+        get_reading = virgo.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'libra':
+        get_reading = libra.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'scorpious':
+        get_reading = scorpious.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'sagittarius':
+        get_reading = sagittarius.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'capricorn':
+        get_reading = capricorn.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'aquarius':
+        get_reading = aquarius.acell('B2').value
+        print(get_reading)
+    elif astro_sign == 'pisces':
+        get_reading = pisces.acell('B2').value
+        print(get_reading)
 
 
 calc_age()
-calc_star_sign(day, month)
-get_today_reading()
+calc_star_sign()
+get_reading()
+
